34f866021937ce625ece6fc52fa444320d940f62
RashmithaEttadi <47919399+RashmithaEttadi@users.noreply.github.com>
2025-05-21 23:03:54 -0400

    index modified

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useRouter } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Platform,
   Text,
@@ -10,13 +10,14 @@ import {
 
 export default function Index() {
   const [date, setDate] = useState(new Date());
-  const [tempDate, setTempDate] = useState(new Date());
   const [showPicker, setShowPicker] = useState(false);
+
   const router = useRouter();
 
-  useEffect(() => {
-    setShowPicker(true);
-  }, []);
+
+  const togglePicker = () => {
+    setShowPicker((prev) => !prev);
+  };
 
   const formatDate = (dateObj: Date) => {
     return dateObj.toLocaleDateString('en-CA');
@@ -28,21 +29,19 @@ export default function Index() {
   };
 
   const onChange = (event: any, selectedDate?: Date) => {
-    if (Platform.OS === 'android') {
       if (event.type === 'set' && selectedDate) {
         setDate(selectedDate);
         navigateToResults(selectedDate);
-      }
       setShowPicker(false);
-    } else if (selectedDate) {
-      setTempDate(selectedDate);
+    } else{
+      togglePicker();
     }
   };
 
   const confirmIOSDate = () => {
-    setDate(tempDate);
+    setDate(date);
     setShowPicker(false);
-    navigateToResults(tempDate);
+    navigateToResults(date);
   };
 
   const cancelIOSDate = () => {
@@ -63,7 +62,7 @@ export default function Index() {
       Select Date
     </Text>
 
-      <TouchableOpacity onPress={() => setShowPicker(true)}>
+      <TouchableOpacity  onPress={togglePicker}>
         <Text className="text-base text-black mb-4 border border-gray-300 px-4 py-2 rounded-md text-center">
           {formatDate(date)}
         </Text>
@@ -72,13 +71,12 @@ export default function Index() {
       {showPicker && (
         <DateTimePicker
           mode="date"
-          display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-          value={Platform.OS === 'ios' ? tempDate : date}
+          display='spinner' 
+          value={date}
           onChange={onChange}
-          themeVariant="light"
           minimumDate={new Date(1900, 1, 15)}
           maximumDate={new Date(2200, 12, 12)}
-          textColor={Platform.OS === 'ios' ? 'black' : undefined}
+          textColor= 'black'
         />
       )}
 
@@ -104,123 +102,3 @@ export default function Index() {
 }
 
 
-
-
-// import React from 'react';
-
-// export default function Index() {
-//   const [date, setDate] = useState(new Date());
-//   const [tempDate, setTempDate] = useState(new Date());
-//   const [showPicker, setShowPicker] = useState(false);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     if (Platform.OS === 'ios') {
-//       setShowPicker(true);
-//     }
-//   }, []);
-
-//   const formatDate = (dateObj: Date) => {
-//     return dateObj.toLocaleDateString('en-CA'); // yyyy-mm-dd
-//   };
-
-//   const navigateToResults = (selectedDate: Date) => {
-//     const formatted = formatDate(selectedDate);
-//     router.push(`/results?date=${formatted}`);
-//   };
-
-//   const onAndroidChange = (
-//     event: any,
-//     selectedDate?: Date
-//   ) => {
-//     if (event.type === 'set' && selectedDate) {
-//       setDate(selectedDate);
-//       navigateToResults(selectedDate);
-//     }
-//   };
-
-//   const showAndroidPicker = () => {
-//     DateTimePickerAndroid.open({
-//       value: date,
-//       mode: 'date',
-//       display: 'spinner',
-//       onChange: onAndroidChange,
-//       is24Hour: false,
-//       minimumDate: new Date(1900, 1, 15),
-//       maximumDate: new Date(2200, 12, 12),
-//     });
-//   };
-
-//   const confirmIOSDate = () => {
-//     setDate(tempDate);
-//     setShowPicker(false);
-//     navigateToResults(tempDate);
-//   };
-
-//   const cancelIOSDate = () => {
-//     setShowPicker(false);
-//   };
-
-//   return (
-//     <View className="flex-1 justify-center px-6">
-
-//       <Text className="text-2xl font-bold text-black mb-2 text-center">
-//         Near Earth Objects(NEO) Monitoring Application
-//       </Text>
-//       <Text className="text-base text-gray-700 mb-6 text-center">
-//         Enter a date to monitor Near-Earth Objects based on their close approach to Earth.
-//       </Text>
-
-//       <Text className="text-lg font-semibold mb-4 text-black">
-//         Select Date
-//       </Text>
-
-//       <TouchableOpacity
-//         onPress={() => {
-//           if (Platform.OS === 'android') {
-//             showAndroidPicker();
-//           } else {
-//             setShowPicker(true);
-//           }
-//         }}
-//       >
-//         <Text className="text-base text-black mb-4 border border-gray-300 px-4 py-2 rounded-md text-center">
-//           {formatDate(date)}
-//         </Text>
-//       </TouchableOpacity>
-
-//       {Platform.OS === 'ios' && showPicker && (
-//         <>
-//           <DateTimePicker
-//             mode="date"
-//             display="spinner"
-//             value={tempDate}
-//             onChange={(_, selectedDate) => {
-//               if (selectedDate) setTempDate(selectedDate);
-//             }}
-//             themeVariant="light"
-//             minimumDate={new Date(1900, 1, 15)}
-//             maximumDate={new Date(2200, 12, 12)}
-//             textColor="black"
-//           />
-
-//           <View className="flex-row justify-between mt-4">
-//             <TouchableOpacity
-//               onPress={cancelIOSDate}
-//               className="bg-gray-300 px-5 py-2 rounded-md"
-//             >
-//               <Text className="text-base text-black font-medium">Cancel</Text>
-//             </TouchableOpacity>
-
-//             <TouchableOpacity
-//               onPress={confirmIOSDate}
-//               className="bg-blue-200 px-5 py-2 rounded-md"
-//             >
-//               <Text className="text-base text-[#075985] font-medium">Confirm</Text>
-//             </TouchableOpacity>
-//           </View>
-//         </>
-//       )}
-//     </View>
-//   );
-// }
